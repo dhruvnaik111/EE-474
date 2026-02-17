f032624d3fa1ef69fd5f737ac1ff73b04cf8ad6a
dhruvnaik111 <68032436+dhruvnaik111@users.noreply.github.com>
2026-02-17 11:48:29 -0800

    part1

--- Naik1_Le2_Lab4/Lab4Part1/Lab4Part1.ino
+++ Naik1_Le2_Lab4/Lab4Part1/Lab4Part1.ino
@@ -4,6 +4,7 @@
 // Description: Manage and schedule tasks with preemptive scheduling algorithms in freeRTOS
 // ==================== Includes ====================
 #include <stddef.h>
+#include <LiquidCrystal_I2C.h>
 
 // ==================== Global Variables ====================
 
@@ -16,30 +17,95 @@ volatile TickType_t remainingLedTime = ledTaskExecutionTime;
 volatile TickType_t remainingCounterTime = counterTaskExecutionTime;
 volatile TickType_t remainingAlphabetTime = alphabetTaskExecutionTime;
 
+// Task Handles
+TaskHandle_t ledTaskHandle;
+TaskHandle_t counterTaskHandle;
+TaskHandle_t alphabetTaskHandle;
+TaskHandle_t schedulerTaskHandle;
+
+//Initialize LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#define LED_PIN 2
+
 // Name: ledTask
 // Description: Blink an LED and update remaining time for this task
 void ledTask(void *arg) {
-
+   while (1) {
+      if(remainingLedTime <= 0) {
+         // Reset remaining time for next round
+         remainingLedTime = ledTaskExecutionTime;
+      } else {
+         digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+         vTaskDelay(250 / portTICK_PERIOD_MS); // Blink every 250 ms
+         remainingLedTime -= 250 / portTICK_PERIOD_MS; // Decrease remaining time
+      }
+   }
 }
 
 // Name: counterTask
 // Description: Print out an incrementing counter to your LCD, and update remaining time for this task
 void counterTask(void *arg) {
+   static int count = 1;
 
+   while(1){
+      if (remainingCounterTime <= 0) {
+         count = 1;
+         remainingCounterTime = counterTaskExecutionTime;
+      } else {
+         lcd.clear();
+         lcd.setCursor(0, 0);
+         static int counter = 0;
+         lcd.print("Count: ");
+         lcd.print(counter++);
+         vTaskDelay(100 / portTICK_PERIOD_MS); 
+         remainingCounterTime -= 100 / portTICK_PERIOD_MS; 
+      }
+   }
 }
 
 // Name: alphabetTask
 // Description: Print out the alphabet to Serial, and update remaining time for this task
 void alphabetTask(void *arg) {
+   char letter = 'A';
 
+   while(1) {
+      if (remainingAlphabetTime <= 0) {
+         remainingAlphabetTime = alphabetTaskExecutionTime;
+      } else {
+         Serial.print(letter++);
+         Serial.print(" ");
+
+         if (letter > 'Z') {
+            letter = 'A';
+         }
+         vTaskDelay(500 / portTICK_PERIOD_MS);
+         remainingAlphabetTime -= 500 / portTICK_PERIOD_MS;
+      }
+   }
 }
 
 // Name: scheduleTasks
-// Description: Implement SRTF scheduling logic. This function should select the task with
+// Description: Implement Shortest Remaining Time First scheduling logic. This function should select the task with
 //              the shortest remaining time and run it. Once a task completes it should
 //              reset its remaining time.
 void scheduleTasks(void *arg) {
-
+   while (1) {
+      if(remainingLedTime < remainingCounterTime && remainingLedTime < remainingAlphabetTime) {
+         vTaskResume(ledTaskHandle);
+         vTaskSuspend(counterTaskHandle);
+         vTaskSuspend(alphabetTaskHandle);
+      } else if(remainingCounterTime < remainingLedTime && remainingCounterTime < remainingAlphabetTime) {
+         vTaskResume(counterTaskHandle);
+         vTaskSuspend(ledTaskHandle);
+         vTaskSuspend(alphabetTaskHandle);
+      } else if(remainingAlphabetTime < remainingLedTime && remainingAlphabetTime < remainingCounterTime) {
+         vTaskResume(alphabetTaskHandle);
+         vTaskSuspend(ledTaskHandle);
+         vTaskSuspend(counterTaskHandle);
+      }
+      vTaskDelay(50 / portTICK_PERIOD_MS);
+   }
 }
 
 
@@ -49,5 +115,24 @@ void setup() {
    //          2. Blink an LED
    //          3. Print a counter to the LCD
    //          4. Print the alphabet to Serial
+
+   //initialize serial monitor
+   Serial.begin(9600);
+   //initialize LCD
+   Wire.begin(8,9);
+   lcd.init();
+   delay(2);
+   lcd.backlight();
+   lcd.clear();
+
+   //tasks
+   xTaskCreatePinnedToCore(scheduleTasks, "scheduleTasks", 1024, NULL, 2, &scheduleTasksHandle, 0); //we want highest priority
+   xTaskCreatePinnedToCore(ledTask, "ledTask", 1024, NULL, 1, &ledTaskHandle, 0);
+   xTaskCreatePinnedToCore(counterTask, "counterTask", 1024, NULL, 1, &counterTaskHandle, 0);
+   xTaskCreatePinnedToCore(alphabetTask, "alphabetTask", 1024, NULL, 1, &alphabetTaskHandle, 0);
+
+   //initialize LED pin
+   pinMode(LED_PIN, OUTPUT);
+
 }
 void loop() {}
