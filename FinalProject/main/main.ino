2843fcf5be46332c5c4330d683af4651ec09a069
dhruvnaik111 <68032436+dhruvnaik111@users.noreply.github.com>
2026-03-05 13:05:13 -0800

    final proj start

---
+++ FinalProject/main/main.ino
@@ -0,0 +1,17 @@
+/**
+ * @file main.ino
+ * @authors Dhruv Naik, Ethan Le
+ * @date 03/12/2026
+ * @brief Something
+ * @details Something
+ */ 
+
+ void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
